1862713f276a651423f3954548c247a3e5a44263
Kuchi Sri Bhargav Ram <55915852+Ram3812@users.noreply.github.com>
2025-09-03 06:29:17 +0000

    Update siteProps with personal information and add hover animation to social icons

--- src/App.jsx
+++ src/App.jsx
@@ -24,14 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Kuchi Ram",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  email: "sbramkuchi@gmail.com",
+  gitHub: "Ram3812",
+  instagram: "r.a.m_007",
+  linkedIn: "Kuchi Sri Bhargav Ram",
   medium: "",
-  twitter: "microsoft",
+  twitter: "ram_3812",
   youTube: "Code",
 };
 
